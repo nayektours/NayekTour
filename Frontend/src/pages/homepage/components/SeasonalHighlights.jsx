9e8ec573850d69166947e5c54f5e324a4c9a49a6
ritesh-intellisys <ritesh.intellisys@gmail.com>
2026-01-19 16:38:13 +0530

    updated statewise package cards design alignment

--- Frontend/src/pages/homepage/components/SeasonalHighlights.jsx
+++ Frontend/src/pages/homepage/components/SeasonalHighlights.jsx
@@ -72,7 +72,7 @@ const SeasonalHighlights = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 md:gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8">
           {seasonalPackages?.map((pkg) =>
           <div
             key={pkg?.id}
--- Frontend/src/pages/homepage/components/StateDiscoveryGrid.jsx
+++ Frontend/src/pages/homepage/components/StateDiscoveryGrid.jsx
@@ -81,7 +81,7 @@ const StateDiscoveryGrid = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8">
           {featuredStates?.map((state) =>
           <div
             key={state?.id}
